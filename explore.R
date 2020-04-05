9137181700cfe3edd5bd7a88008f20b8aeb4b03a
Mike Anderson <ptolemy777@gmail.com>
2020-04-05 11:45:52 -0700

    Download and plot daily epidemic curve updates from Johns Hopkins University

---
+++ explore.R
@@ -0,0 +1,68 @@
+require(data.table)
+require(ggplot2)
+
+setwd('C:\\Users\\hottm\\Desktop\\cv\\Data')
+
+#Download latest data from GitHub
+github_top_dir   = 'raw.githubusercontent.com/CSSEGISandData/COVID-19/master/'
+
+csse_dirpath     = 'csse_covid_19_data/csse_covid_19_time_series/'
+conf_file        = 'time_series_covid19_confirmed_global.csv'
+deaths_file      = 'time_series_covid19_deaths_global.csv'
+rec_file         = 'time_series_covid19_recovered_global.csv'
+
+who_dir          = 'who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/'
+who_file         = 'who_covid_19_sit_rep_time_series.csv'
+
+cur_date = Sys.Date()
+cur_date = paste0(substr(cur_date, 1, 2), substr(cur_date, 6, 7), substr(cur_date, 9, 10))
+insert.date = function(x) gsub('.csv$', paste0('_', cur_date, '.csv'), x)
+
+my.download.file = function(path, filename)
+  download.file(paste0(github_top_dir, path, filename), insert.date(filename), method = 'libcurl')
+my.download.file(csse_dirpath, conf_file)
+my.download.file(csse_dirpath, deaths_file)
+my.download.file(csse_dirpath, rec_file)
+my.download.file(who_dir, who_file)
+
+label = 'Confirmed Cases'
+label = 'Deaths'
+file = conf_file
+file = deaths_file
+
+#Read data
+data = fread(insert.date(file))
+colnames(data) = gsub('^X', '', make.names(colnames(data)))
+
+#Reshape
+data = melt(data, id.vars = colnames(data)[1:4], variable.name = 'Date', value.name = 'Value')
+data[, Date := as.Date(data[, Date], format = '%m.%d.%y')]
+
+#Make all-up numbers for US
+#us_data = data[Country.Region == 'US' & !grepl(',', Province.State) & !is.na(Value),
+#               .(Province.State = '', Country.Region = 'US', Lat = NA, Long = NA, Value=sum(Value)),
+#               by = Date]
+#data = rbind(data, us_data)
+
+#Areas of interest
+aoi = data.table()
+aoi = rbind(aoi, data.table('Hubei', 'China'))
+aoi = rbind(aoi, data.table('', 'Italy'))
+aoi = rbind(aoi, data.table('Washington', 'US'))
+aoi = rbind(aoi, data.table('New York', 'US'))
+aoi = rbind(aoi, data.table('Oregon', 'US'))
+aoi = rbind(aoi, data.table('', 'US'))
+aoi = rbind(aoi, data.table('', 'Germany'))
+aoi = rbind(aoi, data.table('', 'Spain'))
+aoi = rbind(aoi, data.table('', 'Iran'))
+colnames(aoi) = colnames(data)[1:2]
+
+aoi_data = merge(aoi, data, by = c('Province.State', 'Country.Region'))
+aoi_data[, Area := paste0(Province.State, ifelse(Province.State == '', '', ', '), Country.Region)]
+
+#Plot
+p = ggplot(aoi_data[Date >= as.Date('2020-03-01')], aes(x = Date, y = Value, color = Area)) + geom_point()
+p = p + scale_y_log10(breaks = 10^(1:5), labels = c('10', '100', '1k', '10k', '100k'))
+p = p + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+p = p + ylab(label)
+p
---
+++ explore_daily.R
@@ -0,0 +1,98 @@
+#Globals
+require(data.table)
+require(ggplot2)
+
+setwd('C:\\Users\\hottm\\Desktop\\cv\\Data')
+
+#Download latest daily data from GitHub
+github_top_dir = 'raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
+
+cur_date = Sys.Date() - 1
+cur_file = paste0(strftime(cur_date, format = '%m-%d-%Y'), '.csv')
+while (!(cur_file %in% dir('.'))) {
+  download.file(paste0(github_top_dir, cur_file), cur_file, method = 'libcurl')
+  cur_date = cur_date - 1
+  cur_file = paste0(strftime(cur_date, format = '%m-%d-%Y'), '.csv')
+}
+
+#Read data
+daily_files = grep('^([0-9]{2}-){2}[0-9]{4}\\.csv', dir('.'), value = TRUE)
+data = rbindlist(lapply(daily_files, function(file) cbind(file, fread(file))), fill = TRUE)
+
+#Clean
+colnames(data) = make.names(colnames(data))
+
+setnames(data, 'Admin2', 'County')
+colnames(data)[1] = 'Day'
+data[, Day := as.Date(gsub('\\.csv$', '', Day), format = '%m-%d-%Y')]
+
+data[is.na(Country_Region), Country_Region := Country.Region]
+data[grepl('China',   Country_Region), Country_Region := 'China']
+data[grepl('Korea',   Country_Region), Country_Region := 'South Korea']
+data[grepl('Gambia',  Country_Region), Country_Region := 'Gambia']
+data[grepl('Bahamas', Country_Region), Country_Region := 'Bahamas']
+
+data[is.na(Province_State), Province_State := Province.State]
+
+data[, c('Province.State', 'Country.Region', 'Last.Update', 'Latitude',
+         'Longitude', 'FIPS', 'Last_Update', 'Lat', 'Long_', 'Combined_Key')
+     := NULL]
+
+#Reshape
+data = melt(data, measure.vars = c('Confirmed', 'Deaths', 'Recovered', 'Active'),
+            variable.name = 'Metric', value.name = 'Value')
+data[is.na(Value), Value := 0]
+
+#Aggregate numbers by US state, and for the whole US
+data = data[, .(Value = sum(Value)), by = .(Day, Province_State, Country_Region, Metric)]
+us_data = data[Country_Region == 'US', .(Province_State = '', Country_Region = 'US', Value=sum(Value)), by = .(Day, Metric)]
+data = rbind(data, us_data)
+
+#Define and filter down to areas of interest
+aoi = as.data.table(t(data.table(
+  #c('Hubei', 'China'),
+  c('', 'US'),
+  c('', 'Italy'),
+  c('', 'Iran'),
+  c('', 'South Korea'),
+  c('', 'Brazil'),
+  c('', 'Germany'),
+  c('', 'Spain'),
+  c('', 'Sweden')
+  #c('Washington', 'US'),
+  #c('New York', 'US'),
+  #c('New Jersey', 'US'),
+  #c('Oregon', 'US'),
+  #c('Florida', 'US'),
+  #c('Louisiana', 'US'),
+  #c('Michigan', 'US'),
+  #c('Illinois', 'US'),
+  #c('California', 'US'),
+  #c('Massachusetts', 'US')
+)))
+colnames(aoi) = colnames(data)[2:3]
+
+aoi_data = merge(aoi, data, by = c('Province_State', 'Country_Region'))
+aoi_data[, Area := paste0(Province_State, ifelse(Province_State == '', '', ', '), Country_Region)]
+
+#Plot
+cur_metric = 'Confirmed'
+p = ggplot(aoi_data[Day >= as.Date('2020-02-24')][Metric == cur_metric], aes(x = Day, y = Value, color = Area))
+p = p + geom_point() + geom_line()
+p = p + scale_x_date(date_breaks = '1 day', minor_breaks = NULL, date_labels = '%b %d')
+p = p + scale_y_log10(breaks = 10^(1:5), labels = c('10', '100', '1k', '10k', '100k'))
+p = p + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ylab(cur_metric)
+p
+
+p = ggplot(aoi_data[Metric %in% c('Confirmed', 'Deaths')], aes(x = Day, y = Value, color = Metric))
+p = p + facet_wrap(~ Area, scale='free') + geom_point() + geom_line()
+p = p + scale_x_date(date_breaks = '1 week', minor_breaks = NULL, date_labels = '%b %d')
+p = p + scale_y_log10(breaks = 10^(1:5), labels = c('10', '100', '1k', '10k', '100k'))
+p = p + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ylab(cur_metric)
+p
+
+
+x=aoi_data[Country_Region=='US' & Province_State=='Washington' & Metric=='Confirmed', Value]
+y=round((x[2:length(x)]/x[1:(length(x)-1)]-1)*100, digits = 1)
+z=round(sapply(1:(length(y)-6), function(i) mean(y[i:(i+6)])), digits=1)
+plot(z)
